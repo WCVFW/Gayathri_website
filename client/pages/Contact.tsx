299d7c06d7d5face1462910c4aa1446e7ad38427
femivio <femivio2025@gmail.com>
2025-08-01 14:21:51 +0530

    Initial commit

--- client/pages/Contact.tsx
+++ client/pages/Contact.tsx
@@ -72,7 +72,7 @@ export function Contact() {
 //   };
 
   const contactMethods = [
-    { icon: Phone, title: 'Call Us', details: ['+91-9655771091'] },
+    { icon: Phone, title: 'Call Us', details: ['+91-965577109'] },
     { icon: Mail, title: 'Email Us', details: ['deepak@hado.co.in'] },
     { icon: MapPin, title: 'Visit Our Office', details: ['32, 1St Main Road, Ayyappa Nagar, Virugambakkam, Chennai-600092.'] },
   ];
